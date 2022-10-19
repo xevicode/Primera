e9cc872fa06d828735afcbd5d5bcd9b1a66d7496
Alex Escobar Ros <a.escobar.ros@gmail.com>
2022-10-19 09:57:05 +0200

    corretgit

--- introduccion_dart/exercici_03.dart
+++ introduccion_dart/exercici_03.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 void main() {
+  stdout.write('¿Quina multiplicació vols?');
+  int base = int.parse(stdin.readLineSync() ?? '4');
+
   for (int i = 0; i <= 10; i++) {
-    print('index i: ${7 * i}');
+    print('${base} * $i = ${base * i}');
   }
 }
