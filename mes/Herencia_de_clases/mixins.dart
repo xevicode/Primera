89e05d3bac75054ea6673354f12b9187900dd872
Alex Escobar Ros <a.escobar.ros@gmail.com>
2022-10-19 10:42:37 +0200

    xx

--- mes/Herencia_de_clases/mixins.dart
+++ mes/Herencia_de_clases/mixins.dart
@@ -1,29 +1,27 @@
 import 'dart:async';
 
 // mixin -> classe abstracte -> no pot ser incialitzat
-mixin Logger{
-  void imprimir(String texto){
-    final hoy = DateTime.now();    
+mixin Logger {
+  void imprimir(String texto) {
+    final hoy = DateTime.now();
 
     print('$hoy ::: $texto');
-  }  
+  }
 }
-
 
 class Logger2 {
-  void imprimir2(String texto){
+  void imprimir2(String texto) {
     print('hola mundo');
   }
 }
 
-  class Logger3 {
-  void imprimir3(String texto){
+class Logger3 {
+  void imprimir3(String texto) {
     print('hola mundo');
   }
 }
 
-
-class Logger3 extends Logger2 with Logger{}
+class Logger3 extends Logger2 with Logger {}
 
 void main(List<String> args) {
   final astro = Logger();
